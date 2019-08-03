a7c6a74f588d6344b151ae7d92b372b24156f844
Mackmilan <andre.mackmilan@gmail.com>
2019-08-03 10:21:43 -0300

    Components rearangement

---
+++ Sources/UIKit/UIElement.swift
@@ -0,0 +1,18 @@
+//
+//  UIElement.swift
+//  UIKit
+//
+//  Created by Andre Silva on 03/08/19.
+//
+
+import Foundation
+
+public class UIElement: NSObject {
+   
+   var id: String
+   
+   init(id: String) {
+      self.id = id
+   }
+   
+}
