8ddd652499778c98e918d9ef9f217c4103726676
Mackmilan <andre.mackmilan@gmail.com>
2019-08-03 09:48:16 -0300

    First component

---
+++ Sources/UIKit/UIKit.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class UIElement: NSObject {
+
+    var id: String
+
+    init(id: String) {
+        self.id = id
+    }
+
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import UIKitTests
+
+var tests = [XCTestCaseEntry]()
+tests += UIKitTests.allTests()
+XCTMain(tests)
